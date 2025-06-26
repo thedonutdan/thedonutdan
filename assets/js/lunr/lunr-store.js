32254ca09c4487960990ecfb8f3a30e95a6dfef5
thedonutdan <thedonutdan@users.noreply.github.com>
2025-06-26 06:57:41 +0000

    deploy: fd828d03b634f3883eff1cba43e5b4e4d93cd3be

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,12 +2,12 @@ var store = [{
         "title": "Hello World",
         "excerpt":"Alright, first blogpost! I feel it seems sensible to use this space to introduce myself, maybe a little personal elevator pitch, and explain my rationale for setting up this site. I am, as the author card would suggest, Daniel Andrews. I grew up tinkering with computers and dipping my toes...","categories": [],
         "tags": [],
-        "url": "/thedonutdan/hello-world/",
+        "url": "/hello-world/",
         "teaser": null
       },{
         "title": "Asteroids",
         "excerpt":"I embarked on this project after completing my Intro to Java course at Palomar College circa June 2020. We had covered the Java basics and done some static image rendering with Java AWT, but I wanted to dive a little deeper and I was missing the days in middle school...","categories": [],
         "tags": ["Java","AWT","Swing","Asteroids"],
-        "url": "/thedonutdan/projects/asteroids/",
-        "teaser": "/thedonutdan/assets/images/asteroids-crop.png"
+        "url": "/projects/asteroids/",
+        "teaser": "/assets/images/asteroids-crop.png"
       }]
