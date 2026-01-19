76c625f73faea51d41df66bbc4b41fec1ed0518a
dan <dan@debian.attlocal.net>
2026-01-18 22:26:58 -0800

    trying to fix blog script

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,4 +1,4 @@
-fetch('../../blog/posts.json')
+fetch('https://thedonutdan.github.io/thedonutdan/blog/post.json')
   .then(res => res.json())
   .then(posts => {
     const container = document.getElementById('post-list');
